26a77a9d1efe52b944119978d83351ba82d58e3b
danifrakanza@gmail.com <danifrakanza@gmail.com>
2025-05-11 14:55:02 +0700

    Membuat HomeScreen sebagai StatefulWidget untuk mengelola daftar produk

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  final bool isDarkMode;
+  final Function(bool) onToggleTheme;
+
+  const HomeScreen({
+    super.key,
+    required this.isDarkMode,
+    required this.onToggleTheme,
+  });
