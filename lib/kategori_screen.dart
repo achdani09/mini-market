de854b358755fb193f8cbffc0412db3d18523949
danifrakanza@gmail.com <danifrakanza@gmail.com>
2025-05-12 16:26:21 +0700

    Membuat tampilan daftar produk menggunakan ListView

---
+++ lib/kategori_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class KategoriScreen extends StatelessWidget {
+  final List<String> produkList;
+
+  const KategoriScreen({super.key, required this.produkList});
