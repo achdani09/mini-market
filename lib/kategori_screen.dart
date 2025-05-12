1408f1623704b6c7febf73d68803fccbb20f9c2d
danifrakanza@gmail.com <danifrakanza@gmail.com>
2025-05-12 16:29:13 +0700

    Menampilkan pesan jika daftar produk kosong

--- lib/kategori_screen.dart
+++ lib/kategori_screen.dart
@@ -4,3 +4,10 @@ class KategoriScreen extends StatelessWidget {
   final List<String> produkList;
 
   const KategoriScreen({super.key, required this.produkList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Kategori Belanja')),
+      body: produkList.isEmpty
+          ? const Center(child: Text('Belum ada produk ditambahkan.'))
